2abfa9ebf97ed987a0139088a06a318f7be16236
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-02-26 23:12:04 -0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Main from './components/Main';
 
 class App extends Component {
 
     render() {
         return (
-            
+            <Router>
+                <Route exact path="/" component={Main} />
+                {/* <Route path="/about" component={About} />
+                <Route path="/topics" component={Topics} /> */}
+            </Router>
         );
     }
 }
