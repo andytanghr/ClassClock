692f971151fa1c38d5f0132869bdec008a20f13a
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-02-27 23:43:40 -0800

    add schedule data types

---
+++ src/@types/scheduledata.ts
@@ -0,0 +1,24 @@
+export interface School {
+    "fullName": string;
+    "timeZone": string;
+    "schedules": Schedule[];
+    "passingPeriodName": string;
+}
+
+export interface Schedule {
+    "name": string;
+    "days": number[];
+    "classes": Period[];
+}
+
+export interface Period {
+    name: string;
+    startTime: Time;
+    endTime: Time;
+}
+
+export interface Time {
+    hours: number;
+    minutes: number;
+    seconds?: number;
+}
